baa6d39158c37c41a4f9b31ae903b8c4a707b517
moshkito <50770559+moshkito@users.noreply.github.com>
2025-01-04 00:14:39 +0100

    Update server.lua
    Updated to use %

--- server.lua
+++ server.lua
@@ -4,7 +4,7 @@ lib.callback.register('moneywash:washing', function(source, input)
     local src = source
 	local ped = ESX.GetPlayerFromId(src)
 	local count2 = math.floor(input[1])
-	local count = math.floor(input[1] / Config.Prozent)
+	local count = math.floor(input[1] * (1 - Config.Prozent / 100))
 	ped.removeInventoryItem(Config.WashMoney, count2)
 	ped.addInventoryItem(Config.WashedMoney, count)
 end)
